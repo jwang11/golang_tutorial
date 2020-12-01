ad6a6f1483273988912c8c9603737cf6ef343219
root <jing.j.wang@intel.com>
2020-12-01 16:16:50 +0800

    type switch using reflect.TypeOf

--- type_switch/main.go
+++ type_switch/main.go
@@ -28,6 +28,11 @@ type Tiger struct {
 func (self Tiger) shout() string {
     return fmt.Sprintf("hou hou")
 }
+// 定义一个Enum类型
+type Enum int
+const (
+    Zero Enum = 0
+)
 
 func main() {
     var animal Animal = Dog{}
@@ -39,8 +44,8 @@ func main() {
         fmt.Println("animal'type is Cat")
     }
 
-    var animal2 Animal = Cat{}
-    switch a := animal2.(type) {//隐式地为每个case子句声明了一个变量a
+    var animal2  Animal = Cat{}
+    switch a := animal2.(type){//隐式地为每个case子句声明了一个变量a
     case nil: // a的类型是 Animal
         fmt.Println("nil", a)
     case Dog, Cat: // a的类型是 Animal
@@ -49,6 +54,20 @@ func main() {
     case Tiger: // a的类型是 Tiger
         fmt.Println(a.shout(), a.name) // 这里可以直接取出 name 成员
     default: // a的类型是 Animal
-        fmt.Println("default", reflect.TypeOf(a), a)
+        fmt.Println("default")
     }
+
+    var a int
+    typeOfA := reflect.TypeOf(a)
+    fmt.Println(typeOfA.Name(), typeOfA.Kind())
+
+    // 获取结构体实例的反射类型对象
+    typeOfCat := reflect.TypeOf(Cat{})
+    // 显示反射类型对象的名称和种类
+    fmt.Println(typeOfCat.Name(), typeOfCat.Kind())
+    // 获取Zero常量的反射类型对象
+
+    typeOfZ := reflect.TypeOf(Zero)
+    // 显示反射类型对象的名称和种类
+    fmt.Println(typeOfZ.Name(), typeOfZ.Kind())
 }
