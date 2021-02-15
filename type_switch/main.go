cf4b24a50dae31eb3f6ac22b203fb08d3c217690
jwang11 <13162811980@qq.com>
2021-02-16 01:34:55 +0800

    update type_switch

--- type_switch/main.go
+++ type_switch/main.go
@@ -9,13 +9,17 @@ type Animal interface {
     shout() string
 }
 
-type Dog struct {}
+type Dog struct {
+    color string
+}
 
 func (self Dog) shout() string {
     return fmt.Sprintf("wang wang")
 }
 
-type Cat struct {}
+type Cat struct {
+    name string
+}
 
 func (self Cat) shout() string {
     return fmt.Sprintf("miao miao")
@@ -44,15 +48,15 @@ func main() {
         fmt.Println("animal'type is Cat")
     }
 
-    var animal2  Animal = Cat{}
+    var animal2  Animal = Tiger{}
     switch a := animal2.(type){//隐式地为每个case子句声明了一个变量a
     case nil: // a的类型是 Animal
         fmt.Println("nil", a)
-    case Dog, Cat: // a的类型是 Animal
-        fmt.Println(a.shout()) // 输出 {}
-        // fmt.Println(a.name) 这里会报错，因为 Animal 类型没有成员name
     case Tiger: // a的类型是 Tiger
+        a.name = "Tiger"
         fmt.Println(a.shout(), a.name) // 这里可以直接取出 name 成员
+    case Cat, Dog: // 匹配多个Type, a的类型是 Animal
+        fmt.Println(a.shout()) // 输出 {}
     default: // a的类型是 Animal
         fmt.Println("default")
     }
